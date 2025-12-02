4e640ca00209a8e715d9aa68e52f6b2570e7c837
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-12-02 15:34:23 +0100

    chore(ci): build on netlify

--- Package.swift
+++ Package.swift
@@ -13,8 +13,9 @@ let package = Package(
     .library(name: "UnilimCAS", targets: ["UnilimCAS"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Vexcited/Rikka.git", from: "0.1.0"),
-    .package(url: "https://github.com/lachlanbell/SwiftOTP.git", from: "3.0.2"),
+    .package(url: "https://github.com/Vexcited/Rikka", from: "0.1.0"),
+    .package(url: "https://github.com/lachlanbell/SwiftOTP", from: "3.0.2"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.5"),
   ],
   targets: [
     .target(
