77ab26582705731efb084352e557db3c74074f86
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-12-09 10:33:55 +0100

    chore: upgrade iOS to 16

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   name: "Unilim",
   platforms: [
     .macOS(.v13),
-    .iOS(.v15),
+    .iOS(.v16),
   ],
   products: [
     .library(name: "UnilimIUTCS", targets: ["UnilimIUTCS"]),
